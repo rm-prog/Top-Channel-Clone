f5471a3c6246408411a2d28584490b0c567e72e5
Rminollari <rminollari@gmail.com>
2021-01-07 10:40:52 +0100

    draggable news list

---
+++ Scripts/draggable-list-script.js
@@ -0,0 +1,57 @@
+
+let newsIndex = 0;
+let newsImagesInterval;
+
+let imageSize;
+
+$(function () {
+    $(".dots li button").first().css(
+        {
+            background: "white"
+        }
+    );
+    
+    imageSize = screen.width <= 768 ? 300 : 580;
+    newsImagesInterval = setInterval(moveNewsImage, 5000);
+});
+
+function dotClick(b) {
+    $(".dots li button").css(
+        {
+            background: "0 0"
+        }
+    );
+    b.style.background = "white";
+    
+    const newPosition = Number(b.value) * imageSize;
+    newsIndex = parseInt(b.value);
+
+    $("#list-of-news-images a").css(
+        {
+            position: "relative",   
+            right: `${newPosition}px`
+        }
+    );
+    
+    clearInterval(newsImagesInterval);
+    newsImagesInterval = setInterval(moveNewsImage, 5000);
+}
+
+function mouseOverNews() {
+    
+    clearInterval(newsImagesInterval);
+
+}
+
+function mouseOutNews() {
+
+    newsImagesInterval = setInterval(moveNewsImage, 5000);
+
+}
+
+function moveNewsImage() {
+    newsIndex++;
+
+    $(".dots li button")[newsIndex].click();
+        
+}
